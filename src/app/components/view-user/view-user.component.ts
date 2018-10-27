7365deeb518d5b25cd9240120d77f1adf34d5d89
Aditya7898 <aditya.cs.ind@gmail.com>
2018-10-27 19:56:58 +0530

    location

--- src/app/components/view-user/view-user.component.ts
+++ src/app/components/view-user/view-user.component.ts
@@ -42,7 +42,7 @@ export class ViewUserComponent implements OnInit, AfterViewInit {
     this.userService.getUserByName(name).subscribe(data => {
       console.log(data);
       this.user = data.result;
-      this.posts = data.result.posts;
+      this.posts = data.result.posts.reverse();
       this.followers = data.result.followers;
       this.following = data.result.following;
     }, err => console.log(err));
